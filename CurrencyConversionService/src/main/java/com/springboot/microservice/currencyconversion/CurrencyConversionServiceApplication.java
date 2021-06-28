ebb56c429e6b5112a71459fd9e7bb7e7cf13899b
Abinash Malick <abinashmalick12@gmail.com>
2021-06-28 18:41:43 +0530

    Initial commit

---
+++ CurrencyConversionService/src/main/java/com/springboot/microservice/currencyconversion/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.microservice.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.springboot.microservice.currencyconversion")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
